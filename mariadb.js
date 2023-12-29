92ee181177321303a309ae103c32dd801c551417
kim <kajing1077@gmail.com>
2023-12-29 13:07:37 +0900

    feat: Express 앱에서 사용자 및 채널 라우팅 구현

---
+++ mariadb.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'Library',
+    dateStrings: true,
+});
+
+
+module.exports = connection;
