59b2555b61e168dfad0c395addaf760eefdb8c60
kim <kajing1077@gmail.com>
2024-01-14 18:13:00 +0900

    fix: db 경로 수정

--- mariadb.js
+++ mariadb.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'Library',
+    database: 'Bookshop',
     dateStrings: true,
 });
 
