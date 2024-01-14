38742b7d52b2193b5ed85bbab13d004d54887304
kim <kajing1077@gmail.com>
2024-01-14 17:35:39 +0900

    refactor: user 관련 라우팅을 별도의 라우터 모듈로 분리

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+// express 모듈
 const express = require('express');
 const app = express();
-app.listen(3000);
+
+// dotenv 모듈
+const dotenv = require('dotenv');
+dotenv.config();
 
 const userRouter = require('./routes/users');
-// const channelRouter = require('./routes/channels.js');
-//
-app.use('/', userRouter);
-// app.use('/channels', channelRouter); // 공통된 url 빼주기
+
+app.use('/users', userRouter);
+
+app.listen(3000);
--- routes/users.js
+++ routes/users.js
@@ -1,45 +1,18 @@
 const express = require('express');
-const router = express.Router(); // 라우터로 활용 가능
-const conn = require('../mariadb.js');
+const router = express.Router();
+const {
+    join,
+    login,
+    passwordResetRequest,
+    passwordReset
+} = require('../controller/UserController');
+
 router.use(express.json());
 
-router.post('/login', (req, res) => {
-    const { email, password } = req.body;
+router.post('/join', join);
+router.post('/login', login);
+router.post('/reset', passwordResetRequest);
+router.put('/reset', passwordReset);
 
-    let sql = `SELECT * FROM users WHERE email = ?`
-    conn.query(sql, email,
-        function (err, results) {
-            let loginUser = results[0];
-
-            if (loginUser && loginUser.password === password) {
-                res.status(200).json({
-                    message: `${ loginUser.email } 님 로그인 되었습니다.`
-                })
-            } else {
-                res.status(404).json({
-                    message: "이메일 또는 비밀번호가 틀렸습니다."
-                })
-            }
-        }
-    )
-})
-
-router.post('/join', (req, res) => {
-    if (req.body === {}) {
-        res.status(400).json({
-            message: `입력값을 다시 확인해주세요.`
-        })
-    } else {
-        const { email, password } = req.body;
-
-        let sql = `INSERT INTO users (email, password) VALUES (?, ?)`
-        let values = [email, password];
-        conn.query(sql, values,
-            function (err, results) {
-                res.status(201).json(results);
-            }
-        )
-    }
-})
 
 module.exports = router;
