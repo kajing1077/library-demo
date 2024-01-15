667ce3d1356241f0394aa8950b98668c009d8d83
kim <kajing1077@gmail.com>
2024-01-15 12:45:36 +0900

    refactor: order 관련 라우팅을 별도의 라우터 모듈로 분리

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ const bookRouter = require('./routes/books');
 const categoryRouter = require('./routes/category');
 const cartRouter = require('./routes/carts');
 const likeRouter = require('./routes/likes');
+const orderRouter = require('./routes/orders');
 
 app.use('/users', userRouter);
 app.use('/books', bookRouter);
 app.use('/category', categoryRouter);
 app.use('/carts', cartRouter);
 app.use('/likes', likeRouter);
+app.use('orders', orderRouter);
 
 app.listen(process.env.PORT);
