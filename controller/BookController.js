ac0f08e1378783fe905069ad908f703a4ec0af7c
kim <kajing1077@gmail.com>
2024-01-16 15:00:34 +0900

    feat: 에러 및 응답 처리를 모듈화하여 추가

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,49 +1,123 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
-const { createAllBooksQuery, createBookDetailQuery } = require("../utils/bookQueries");
+const ensureAuthorization = require('../auth');
+const jwt = require("jsonwebtoken");
+const { handleDatabaseError } = require("../utils/errorHandler");
+const { sendResponse } = require("../utils/responseHandler");
 
 const allBooks = (req, res) => {
+    let allBooksRes = {};
     let { category_id, news, limit, currentPage } = req.query;
-    let offset = 0;
+    let offset = limit * (currentPage - 1);
 
-    if (limit && currentPage) {
-        limit = parseInt(limit);
-        currentPage = parseInt(currentPage);
-        offset = limit * (currentPage - 1);
+    let sql = "SELECT SQL_CALC_FOUND_ROWS *, (SELECT count(*) FROM Bookshop.likes WHERE Bookshop.books.id = Bookshop.likes.liked_book_id) AS likes FROM books";
+    let values = [];
+
+    if (category_id && news) {
+        sql += ` WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+        values = [category_id];
+    } else if (category_id) {
+        sql += ` WHERE category_id=?`;
+        values = [category_id];
+    } else if (news) {
+        sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
     }
 
-    const { sql, values } = createAllBooksQuery({ category_id, news, limit, offset });
+    sql += " LIMIT ? OFFSET ?";
+    values.push(parseInt(limit), offset);
 
-    conn.query(sql, values, (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
+    conn.query(sql, values,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return handleDatabaseError(err, res);
+            }
 
-        if (results.length) {
-            return res.status(StatusCodes.OK).json(results);
-        } else {
-            return res.status(StatusCodes.NOT_FOUND).end();
-        }
-    });
+            if (results.length) {
+                results.map(function (result) {
+                    result.pubDate = result.pub_date;
+                    delete result.pub_date;
+                });
+                allBooksRes.books = results;
+            } else {
+                return sendResponse(res, StatusCodes.NOT_FOUND);
+            }
+        });
+
+    sql = "SELECT found_rows()";
+
+    conn.query(sql,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return handleDatabaseError(err, res);
+            }
+
+            let pagination = {};
+            pagination.currentPage = parseInt(currentPage);
+            pagination.totalCount = results[0]["found_rows()"];
+
+            allBooksRes.pagination = pagination;
+
+            return sendResponse(res, StatusCodes.OK, allBooksRes);
+
+        });
 }
+
+
 const bookDetail = (req, res) => {
-    let { user_id } = req.body;
-    let book_id = parseInt(req.params.id);
+    let authorization = ensureAuthorization(req, res);
 
-    const { sql, values } = createBookDetailQuery({ user_id, book_id });
+    if (authorization instanceof jwt.TokenExpiredError) {
+        return sendResponse(res, StatusCodes.UNAUTHORIZED, { 'message': '로그인 세션이 만료되었습니다.' });
+    } else if (authorization instanceof jwt.JsonWebTokenError) {
+        return sendResponse(res, StatusCodes.BAD_REQUEST, { 'message': '잘못된 토큰입니다.' });
+    } else if (authorization instanceof ReferenceError) {
+        let book_id = req.params.id;
+        let sql = `SELECT *,
+                          (SELECT count(*) FROM Bookshop.likes WHERE liked_book_id = books.id) AS likes
+                   FROM Bookshop.books
+                            LEFT JOIN Bookshop.category
+                                      ON books.category_id = category.category_id
+                   WHERE books.id = ?`;
+        let values = [book_id];
+        conn.query(sql, values, (err, results) => {
+            if (err) {
+                console.log(err);
+                return handleDatabaseError(err, res);
+            }
+            if (results[0]) {
+                return sendResponse(res, StatusCodes.OK, results[0]);
+            } else {
+                return sendResponse(res, StatusCodes.NOT_FOUND);
+            }
+        })
+    } else {// 로그인 상태
+        let book_id = parseInt(req.params.id);
 
-    conn.query(sql, values, (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
-        if (results[0]) {
-            res.status(StatusCodes.OK).json(results[0]);
-        } else {
-            return res.status(StatusCodes.NOT_FOUND).end();
-        }
-    })
+        let sql = `SELECT *,
+                          (SELECT count(*) FROM Bookshop.likes WHERE liked_book_id = books.id) AS likes,
+                          (SELECT EXISTS (SELECT 1
+                                          FROM Bookshop.likes
+                                          WHERE likes.user_id = ?
+                                            AND likes.liked_book_id = ?))                      AS liked
+                   FROM Bookshop.books
+                            LEFT JOIN Bookshop.category
+                                      ON books.category_id = category.category_id
+                   WHERE books.id = ?`;
+        let values = [authorization.id, book_id, book_id];
+        conn.query(sql, values, (err, results) => {
+            if (err) {
+                console.log(err);
+                return handleDatabaseError(err, res);
+            }
+            if (results[0]) {
+                return sendResponse(res, StatusCodes.OK, results[0]);
+            } else {
+                return sendResponse(res, StatusCodes.NOT_FOUND);
+            }
+        })
+    }
 }
 
 
