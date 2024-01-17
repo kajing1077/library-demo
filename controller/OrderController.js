038d061a40e79d4cfd2022698018b925d684329d
kim <kajing1077@gmail.com>
2024-01-17 12:18:00 +0900

    feat: 주문 조회 및 상세 조회할때 jwt 확인 추가 및 body key 값 camelCase 변경 함수 추가

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,5 +1,8 @@
 const { StatusCodes } = require("http-status-codes");
 const mariadb = require("mysql2/promise");
+const { orderQueries } = require("../utils/dbQueries");
+const ensureAuthorization = require('../auth');
+const jwt = require("jsonwebtoken");
 
 
 const order = async (req, res) => {
@@ -11,103 +14,135 @@ const order = async (req, res) => {
         dateStrings: true,
     });
 
+    let authorization = ensureAuthorization(req, res);
 
-    const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } = req.body;
-
-
-    // delivery 테이블 삽입
-    let sql = `INSERT INTO Bookshop.delivery (address, receiver, contact)
-               VALUES (?, ?, ?)`;
-    let values = [delivery.address, delivery.receiver, delivery.contact];
-    let [results] = await conn.execute(sql, values);
-
-    let delivery_id = results.insertId;
-
-
-    // orders 테이블 삽입
-    sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
-           VALUES (?, ?, ?, ?, ?)`;
-    values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-    [results] = await conn.execute(sql, values);
-    let order_id = results.insertId;
-
-    console.log(results);
+    if (authorization instanceof jwt.TokenExpiredError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            'message': '로그인 세션이 만료되었습니다.'
+        });
+    } else if (authorization instanceof jwt.JsonWebTokenError) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            'message': '잘못된 토큰입니다.'
+        });
+    } else { // 유효한 토큰이면
+        const { items, delivery, totalQuantity, totalPrice, firstBookTitle } = req.body;
 
 
-    // items를 가지고, 장바구니에서 book_id, quantity 조회
-    sql = `SELECT book_id, quantity
-           FROM cartItems
-           WHERE id IN (?)`
-    let [orderItems, fields] = await conn.query(sql, [items]);
+        // delivery 테이블 삽입
+        let sql = orderQueries.insertNewDeliveryInfo
+        let values = [delivery.address, delivery.receiver, delivery.contact];
+        let [results] = await conn.execute(sql, values);
 
-    // orderedBook 테이블 삽입
-    sql = `INSERT INTO orderedBook (order_id, book_id, quantity)
-           VALUES ?`;
+        let delivery_id = results.insertId;
 
-    // items.. 배열 : 요소들을 하나씩 꺼내서 forEach문을 돌려서 >
-    values = []; // 초기화.
-    orderItems.forEach((item) => {
-        values.push([order_id, item.book_id, item.quantity]);
-    })
 
-    results = await conn.query(sql, [values]);
+        // orders 테이블 삽입
+        sql = orderQueries.insertNewOrder;
+        values = [firstBookTitle, totalQuantity, totalPrice, authorization.id, delivery_id];
+        [results] = await conn.execute(sql, values);
+        let order_id = results.insertId;
 
-    let result = await deleteCartItems(conn, items);
+        console.log(results);
 
-    return res.status(StatusCodes.OK).json(result);
+
+        // items를 가지고, 장바구니에서 book_id, quantity 조회
+        sql = orderQueries.selectCartItemsForOrder;
+        let [orderItems, fields] = await conn.query(sql, [items]);
+
+        // orderedBook 테이블 삽입
+        sql = orderQueries.insertOrderedBooks;
+
+        values = []; // 초기화.
+        orderItems.forEach((item) => {
+            values.push([order_id, item.book_id, item.quantity]);
+        })
+
+        // results = await conn.query(sql, [values]);
+        let result = await deleteCartItems(conn, items);
+
+        return res.status(StatusCodes.OK).json(result);
+    }
 };
 const deleteCartItems = async (conn, items) => {
-    let sql = `DELETE
-               FROM cartItems
-               WHERE id IN (?)`;
+    let sql = orderQueries.removeAllSelectedCartItems;
 
     let result = await conn.query(sql, [items]);
     return result;
 
 }
 
-
 const getOrders = async (req, res) => {
-    const conn = await mariadb.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'Bookshop',
-        dateStrings: true,
-    });
-    sql = `SELECT orders.id,
-                  created_at,
-                  address,
-                  receiver,
-                  contact,
-                  book_title,
-                  total_quantity,
-                  total_price
-           FROM orders
-                    LEFT JOIN delivery
-                              ON orders.delivery_id = delivery.id;`
-    let [rows, field] = await conn.query(sql);
-    return res.status(StatusCodes.OK).json(rows);
+    const authorization = ensureAuthorization(req, res);
+
+    if (authorization instanceof jwt.TokenExpiredError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            'message': '로그인 세션이 만료되었습니다.'
+        });
+    } else if (authorization instanceof jwt.JsonWebTokenError || authorization instanceof ReferenceError) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            'message': '잘못된 토큰입니다.'
+        });
+    } else { // 유효한 토큰이면
+        const conn = await mariadb.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'Bookshop',
+            dateStrings: true,
+        });
+
+        let sql = orderQueries.fetchOrderById;
+
+
+        //let sql = orderQueries.fetchOrdersByUserId;
+        let [rows, field] = await conn.query(sql, [authorization.id]);
+
+        rows = rows.map(result => convertSnakeToCamel(result));
+
+        return res.status(StatusCodes.OK).json(rows);
+    }
 };
-
-
 const getOrderDetail = async (req, res) => {
-    const { id } = req.params;
 
-    const conn = await mariadb.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'Bookshop',
-        dateStrings: true,
-    });
+    const authorization = ensureAuthorization(req, res);
 
-    let sql = `SELECT book_id, title, author, price, quantity
-               FROM orderedBook
-                        LEFT JOIN books ON orderedBook.book_id = books.id
-               WHERE order_id = ?`;
-    let [rows, fields] = await conn.query(sql, [id]);
-    return res.status(StatusCodes.OK).json(rows);
+    if (authorization instanceof jwt.TokenExpiredError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            'message': '로그인 세션이 만료되었습니다.'
+        });
+    } else if (authorization instanceof jwt.JsonWebTokenError || authorization instanceof ReferenceError) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            'message': '잘못된 토큰입니다.'
+        });
+    } else { // 유효한 토큰이면
+        const orderId = req.params.id;
+        const conn = await mariadb.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'Bookshop',
+            dateStrings: true,
+        });
+
+        let sql = orderQueries.fetchOrderDetailsByIdAndUserId;
+        let values = [orderId, authorization.id];
+        let [rows, fields] = await conn.query(sql, values);
+
+
+        rows = rows.map(result => convertSnakeToCamel(result));
+
+        return res.status(StatusCodes.OK).json(rows);
+    }
+}
+
+
+const convertSnakeToCamel = (obj) => {
+    const camelObj = {};
+    for (const key in obj) {
+        const camelKey = key.replace(/_(\w)/g, (_, letter) => letter.toUpperCase());
+        camelObj[camelKey] = obj[key];
+    }
+    return camelObj;
 };
 
 module.exports = {
