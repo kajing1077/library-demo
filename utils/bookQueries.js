cb52f2e1d9c5d71cc9181f47d4709781345b318b
kim <kajing1077@gmail.com>
2024-01-17 12:37:58 +0900

    feat: 도서 상세 정보 쿼리 생성 기능 추가

--- utils/bookQueries.js
+++ utils/bookQueries.js
@@ -1,39 +1,26 @@
-function createAllBooksQuery({ category_id, news, limit, offset }) {
-    let sql = "SELECT *, (SELECT count(*) FROM Bookshop.likes WHERE Bookshop.books.id = Bookshop.likes.liked_book_id) AS likes FROM books";
-    let values = [];
-
-    if (category_id && news) {
-        sql += ` WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
-        values = [category_id];
-    } else if (category_id) {
-        sql += ` WHERE category_id=?`;
-        values = [category_id];
-    } else if (news) {
-        sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
-    }
-
-    if (limit) {
-        sql += ` LIMIT ? OFFSET ?`;
-        values.push(limit, offset);
-    }
-
-    return { sql, values };
-}
-
-function createBookDetailQuery({ user_id, book_id }) {
+function createBookDetailQuery({ user_id, book_id, is_logged_in }) {
     let sql = `SELECT *,
-                   (SELECT count(*) FROM Bookshop.likes WHERE liked_book_id = books.id) AS likes,
-                   (SELECT EXISTS (SELECT 1 FROM Bookshop.likes WHERE likes.user_id = ? AND likes.liked_book_id = ?)) AS liked
-               FROM Bookshop.books
-               LEFT JOIN Bookshop.category
-               ON books.category_id = category.category_id
-               WHERE books.id = ?`;
-    let values = [user_id, book_id, book_id];
+                      (SELECT count(*) FROM Bookshop.likes WHERE liked_book_id = books.id) AS likes`;
+
+    if (is_logged_in) {
+        sql += `, (SELECT EXISTS (SELECT 1 FROM Bookshop.likes WHERE likes.user_id = ? AND likes.liked_book_id = ?)) AS liked`;
+    }
+
+    sql += `
+              FROM Bookshop.books
+              LEFT JOIN Bookshop.category
+              ON books.category_id = category.category_id
+              WHERE books.id = ?`;
+
+    let values = [book_id];
+
+    if (is_logged_in) {
+        values.push(user_id, book_id);
+    }
 
     return { sql, values };
 }
 
 module.exports = {
-    createAllBooksQuery,
     createBookDetailQuery
 };
